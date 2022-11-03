692b08ae2bac1152f1dd9a956e9f17a2430d2f32
edcilo <edcilop@gmail.com>
2022-11-03 10:45:20 -0600

    feat(plugin): lsp, cmp, mason, prettier

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,4 +43,20 @@ packer.startup(function(use)
     }
     use "p00f/nvim-ts-rainbow"
     use 'windwp/nvim-ts-autotag'
+
+    -- Completion
+    use 'onsails/lspkind-nvim'              -- Add vscode-like pictograms
+    use 'hrsh7th/cmp-nvim-lsp'              -- Source for neovim's built-in language server client
+    use 'hrsh7th/cmp-buffer'                -- Source for buffer words
+    use 'hrsh7th/nvim-cmp'                  -- A completion engine
+
+    use 'L3MON4D3/LuaSnip'                  -- Snippets
+
+    -- LSP and linters
+    use 'neovim/nvim-lspconfig'             -- LSP (language server protocol)
+    use 'glepnir/lspsaga.nvim'              -- LSP UIs
+    use 'jose-elias-alvarez/null-ls.nvim'   -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+    use 'williamboman/mason.nvim'           -- Easily install and manage LSP servers, DAP servers, linters and formatters
+    use 'williamboman/mason-lspconfig.nvim' -- bridge mason with the lspconfig plugin
+    use "MunifTanjim/prettier.nvim"         -- Prettier for Neovim
 end)
