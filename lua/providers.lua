c85df96db7ac0eecdc73a2894ef2ea1693a9bbf0
edcilo <edcilop@gmail.com>
2022-11-03 15:47:47 -0600

    feat(providers): add ruby and node providers

--- lua/providers.lua
+++ lua/providers.lua
@@ -1,2 +1,4 @@
 vim.g.python3_host_prog = '/opt/homebrew/bin/python3.9'
+vim.g.ruby_host_prog = '/usr/local/bin/neovim-ruby-host'
+vim.g.node_host_prog = '/Users/edcilo/.nvm/versions/node/v17.3.0/lib/node_modules/neovim/bin/cli.js'
 vim.g.loaded_perl_provider = 0
