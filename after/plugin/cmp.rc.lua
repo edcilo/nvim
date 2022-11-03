e5340a1764cbde502535220601b6926130a2353f
edcilo <edcilop@gmail.com>
2022-11-03 13:55:56 -0600

    feat(cmp): set configuration

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -27,3 +27,9 @@ cmp.setup({
     format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
   }
 })
+
+vim.cmd [[
+  set completeopt=menuone,noinsert,noselect
+  highlight! default link CmpItemKind CmpItemMenuDefault
+]]
+
