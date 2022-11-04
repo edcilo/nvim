e195032ca3b632b81830739cf99412fcbae1d0a7
edcilo <edcilop@gmail.com>
2022-11-03 19:09:10 -0600

    feat(plugin): diffview

--- after/plugin/neogit.rc.lua
+++ after/plugin/neogit.rc.lua
@@ -1,4 +1,31 @@
 local status, neogit = pcall(require, 'neogit')
 if (not status) then return end
 
-neogit.setup()
+neogit.setup({
+  integrations = {
+    diffview = true
+  },
+  sections = {
+    untracked = {
+      folded = false,
+    },
+    unstaged = {
+      folded = false,
+    },
+    staged = {
+      folded = false,
+    },
+    stashes = {
+      folded = true,
+    },
+    unpulled = {
+      folded = true,
+    },
+    unmerged = {
+      folded = true,
+    },
+    recent = {
+      folded = true,
+    },
+  }
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,6 +21,7 @@ packer.startup(function(use)
     use 'norcalli/nvim-colorizer.lua'
     use 'lewis6991/gitsigns.nvim'
     use 'TimUntersberger/neogit'
+    use 'sindrets/diffview.nvim'
     use 'windwp/nvim-autopairs'
     use 'nvim-telescope/telescope-file-browser.nvim'
     use 'nvim-telescope/telescope.nvim'
